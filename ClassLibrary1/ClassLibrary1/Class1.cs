c7a2c3ae311f396507b46030d48fb107185f221b
Hatim Abahri <hatimabahri9@gmail.com>
2023-10-21 14:07:06 +0200

    i premier  commit i premier  commii premier  coi premier  commitmmitt#

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ClassLibrary1
+{
+    public class book
+    {
+        public string Titel;
+        public string Autor;
+        public int Pages;
+
+    }
+}
